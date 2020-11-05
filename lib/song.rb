0fea91b89236efe724853d52540f7b2249480693
your_name <your_email@example.com>
2020-11-05 13:43:28 -0500

    commit tree 995549780619e654118533fbc1a9468f42c588d7

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,72 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song= self.new
+    self.all << song
+    song
+  end 
+
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end 
+
+  def self.create_by_name(title)
+    song= self.create
+    song.name = title 
+    song
+  end
+
+  def self.find_by_name(title)
+    self.all.detect do |song|
+      if song.name == title 
+        true
+        song.name
+      end 
+    end 
+  end 
+
+
+  def self.find_or_create_by_name(title) 
+    #returns iesisting song or creates a new song if it doesn't exist.
+     user_input=self.find_by_name(title)
+      if user_input
+        true 
+        user_input 
+      else 
+        self.create_by_name(title)
+      end 
+    end
+   
+    def self.alphabetical
+      sort= self.all.sort_by do |song|
+        song.name  
+      end 
+      sort
+    end 
+
+    def self.new_from_filename(filename)
+      song_array = filename.split(" - ")
+      song_array[1] = song_array[1].chomp(".mp3")
+      song = self.new
+      song.name = song_array[1]
+      song.artist_name = song_array[0]
+      song
+    end
+  
+    def self.create_from_filename(filename)
+      result = self.new_from_filename(filename)
+      song = self.create
+      song.name = result.name
+      song.artist_name = result.artist_name
+      song
+    end
+  
+    def self.destroy_all
+      self.all.clear
+    end
+  
+
 end
